1aa8fb7dd01ee3b64ac69a28216d534b52b86659
Nelly2015 <nelly8508@mail.ru>
2023-09-10 21:05:57 +0300

    Правка радиокнопок и чекбоксов

--- build/js/script.js
+++ build/js/script.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('.navigation');
 const navBtn = document.querySelector('.navigation__toggle');
 
 if (document.querySelector('.navigation--no-js')) {
-  document.querySelector('.navigation--no-js').classList.remove('.navigation--no-js');
+  document.querySelector('.navigation--no-js').classList.remove('navigation--no-js');
 }
 
 if (navBtn) {
