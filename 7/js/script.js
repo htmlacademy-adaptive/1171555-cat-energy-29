d93dc1e18309ea3f85fe8e03694a10de1c5aacea
Keks <keks@htmlacademy.ru>
2023-09-18 23:00:34 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/script.js
+++ 7/js/script.js
@@ -1,8 +1,9 @@
 const nav = document.querySelector('.navigation');
 const navBtn = document.querySelector('.navigation__toggle');
 
-if (document.querySelector('.navigation--no-js')) {
-  document.querySelector('.navigation--no-js').classList.remove('navigation--no-js');
+
+if (document.querySelector('.no-js')) {
+  document.querySelector('.no-js').classList.remove('no-js');
 }
 
 if (navBtn) {
